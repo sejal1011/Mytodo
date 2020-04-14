37fe91ddebe06117d08b88d4f598ec5c919355e4
sejal maniyar <sejal1011maniyar@gmail.com>
2020-04-15 01:29:36 +0530

    Added a Procfile.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
+const _ = require("lodash");
 
 const app = express();
 
@@ -27,7 +28,15 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://sejal1011maniyar:sejal1011maniyar@cluster0-iufgx.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
@@ -92,45 +101,45 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-app.get("/secrets", function(req, res){
-  User.find({"secret": {$ne: null}}, function(err, foundUsers){
-    if (err){
-      console.log(err);
-    } else {
-      if (foundUsers) {
-        res.render("secrets", {usersWithSecrets: foundUsers});
-      }
-    }
-  });
-});
+//app.get("/secrets", function(req, res){
+//  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+//    if (err){
+//      console.log(err);
+//    } else {
+//      if (foundUsers) {
+//        res.render("secrets", {usersWithSecrets: foundUsers});
+//      }
+//    }
+//  });
+//});
 
-app.get("/submit", function(req, res){
-  if (req.isAuthenticated()){
-    res.render("submit");
-  } else {
-    res.redirect("/login");
-  }
-});
+//app.get("/submit", function(req, res){
+//  if (req.isAuthenticated()){
+//    res.render("submit");
+//  } else {
+//    res.redirect("/login");
+//  }
+//});
 
-app.post("/submit", function(req, res){
-  const submittedSecret = req.body.secret;
-
-//Once the user is authenticated and their session gets saved, their user details are saved to req.user.
-  // console.log(req.user.id);
-
-  User.findById(req.user.id, function(err, foundUser){
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        foundUser.secret = submittedSecret;
-        foundUser.save(function(){
-          res.redirect("/secrets");
-        });
-      }
-    }
-  });
-});
+//app.post("/submit", function(req, res){
+//  const submittedSecret = req.body.secret;
+//
+////Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+//  // console.log(req.user.id);
+//
+//  User.findById(req.user.id, function(err, foundUser){
+//    if (err) {
+//      console.log(err);
+//    } else {
+//      if (foundUser) {
+//        foundUser.secret = submittedSecret;
+//        foundUser.save(function(){
+//          res.redirect("/secrets");
+//        });
+//      }
+//    }
+//  });
+//});
 
 app.get("/logout", function(req, res){
   req.logout();
@@ -171,12 +180,145 @@ app.post("/login", function(req, res){
 
 });
 
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+
+app.get("/", function(req, res) {
+
+  Item.find({}, function(err, foundItems){
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully savevd default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {listTitle: "date1", newListItems: foundItems});
+    }
+  });
+
+});
+
+app.get("/:customListName", function(req, res){
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if (!foundList){
+        //Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        //Show an existing list
+
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+
+
+
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+  const item = new Item({
+    name: itemName
+  });
+const birthday = new Date('August 19, 1975');
+const date1 = birthday.getDate();
+  if (listName === "date1"){
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+
+
+  if (listName === "date1") {
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if (!err) {
+        console.log("Successfully deleted checked item.");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if (!err){
+        res.redirect("/" + listName);
+      }
+    });
+  }
+
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.get("/list",function(req,res){
+    res.render("list");
+});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
 
 
 
 
-app.listen(3000, function() {
+
+
+app.listen(port, function() {
   console.log("Server started on port 3000.");
 });
